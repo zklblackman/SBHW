a8585128328392dd444628964c9516cc054b3a4e
zklblackman <zklblackman@git.com>
2018-08-20 00:09:13 +0800

    hw

---
+++ src/test/java/com/zklblackman/hw/hw/HwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zklblackman.hw.hw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HwApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
